8f6cc5fe3ac5b4479994abe31c241a4012a638c6
Blake Stoner <stonerblake@gmail.com>
2019-04-19 09:37:48 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      #your key:patriarch
+:matriarch
+:hero
+:hero_friends/value pairs here!
       }, 
     :capulet => {
       #your key/value pairs here!
